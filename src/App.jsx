92c81a82594aa08779a1da33fffb96351c86dc05
rubenledesma10 <rubenledesma89@gmail.com>
2026-04-21 22:10:23 -0300

    fix navbar listas

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Lista from "./pages/Lista";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +13,7 @@ function App() {
   return (
    <Routes>
       <Route path='/' element={<Home />} /> //cuando la url es /, muestra la home
-
+      <Route path='/listas' element={<Lista />}/> 
     </Routes>
   )
 }
