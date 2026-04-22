fca94181e1e968c73cb52ed02a429992d4967b04
SantiDev03 <romanosantiagonicolas@gmail.com>
2026-04-21 23:45:10 -0300

    Actualizamos y comentamos la rama feature/renderizadocondicional, luego hacemos un push para subirla a github y solicitamos el pull request desde feature/renderizadocondicional hacia developer

--- src/pages/Tarea.jsx
+++ src/pages/Tarea.jsx
@@ -3,14 +3,22 @@ import "./Tarea.css";
 
 function Tarea() {
   const [tareas, setTareas] = useState([
+    // Por ejemplo, ahora nos muestra las tareas y su estado, si nosotros
+    // borramos/comentamos las constantes, nos muestra el mensaje de NO hay tareas
     { text: "Aprender React", completed: true },
     { text: "Hacer ejercicio", completed: false },
   ]);
 
+  // Esta función cambia el estado de la tarea (true o false)
   const toggleTarea = (index) => {
     const nuevasTareas = tareas.map((t, i) =>
-      i === index ? { ...t, completed: !t.completed } : t,
+      // map recorre el array viendo el contenido de las constantes
+      // t es cada tarea e i su índice
+      i === index
+        ? { ...t, completed: !t.completed } // cambia el estado de la tarea
+        : t,
     );
+
     setTareas(nuevasTareas);
   };
 
@@ -19,22 +27,30 @@ function Tarea() {
       <h1>Lista de Tareas</h1>
 
       {tareas.length === 0 ? (
+        // Compara la longitud de la constante tareas, si es 0
+        // nos muestra el mensaje de No hay tareas
         <p>No hay tareas</p>
       ) : (
         tareas.map((t, i) => (
+          // Con key={i} identificamos el elemento
           <div className="tarea" key={i}>
             <p
+              // Si la tarea está en true, se tacha el texto
               style={{ textDecoration: t.completed ? "line-through" : "none" }}
             >
               {t.text}
             </p>
 
+            {/* && compara, es un if pero más corto, entonces
+            si la tarea está en true, renderiza "Completada", sino no */}
             {t.completed && <span className="completada">✔ Completada</span>}
 
+            {/* Botón que cambia el estado de la tarea */}
             <button
               className={t.completed ? "btn-cancelar" : "btn-realizar"}
               onClick={() => toggleTarea(i)}
             >
+              {/* Si está en true cancelar tarea, sino realizar tarea */}
               {t.completed ? "Cancelar tarea" : "Realizar tarea"}
             </button>
           </div>
